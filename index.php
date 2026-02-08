3267aff524fdecf5375f2ad0bd5d9370ec0b7863
Breno santos <Breno.ds263@gmail.com>
2026-02-08 16:49:41 -0300

    Feat: criada conexão

--- index.php
+++ index.php
@@ -18,4 +18,19 @@ $username = "root";
 $password = "Senha123";
 $database = "meubanco";
 
-// Criar conexão
+<?php
+$servername = getenv('DB_HOST') ?: "db_host";
+$username   = getenv('DB_USER') ?: "root";
+$password   = getenv('DB_PASS') ?: "Senha123";
+$database   = getenv('DB_NAME') ?: "meubanco";
+
+$link = new mysqli($servername, $username, $password, $database);
+
+if ($link->connect_error) {
+    die("Connection failed: " . $link->connect_error);
+}
+?>
+<style>
+    body { background-color: #1776f6; color: white; font-family: sans-serif; }
+    .header { color: #ff8000; font-weight: bold; }
+</style>
